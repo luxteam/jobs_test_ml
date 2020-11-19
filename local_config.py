7ea8968ac022a36c0c726a8498713fc1e2e01f19
Ilia Nemankov <INemankov@luxoft.com>
2020-11-19 10:31:37 +0300

    Update local_config

--- local_config.py
+++ local_config.py
@@ -1,5 +1,8 @@
 tool_name = 'ml'
-report_type = 'ml'
+report_type = 'perf'
 show_skipped_groups = True
-tracked_metrics = {}
-tracked_metrics_files_number = 10
+tracked_metrics = {
+    'test_status': {'metric_name': 'passed', 'displaying_name': 'Passed tests number', 'function': 'match', 'pattern': 'passed', 'displaying_unit': '', 'separation_field': 'tool', 'display_zeros': True},
+    'render_time': {'displaying_name': 'Render time', 'function': 'sum', 'displaying_unit': 's', 'separation_field': 'tool'}
+}
+tracked_metrics_files_number = 10000
