a1ac24e136440aae9391f951bed56d33910cdef2
Ilia Nemankov <INemankov@luxoft.com>
2020-11-20 17:14:13 +0300

    Rename 'Render time' to 'Inference Time'

--- local_config.py
+++ local_config.py
@@ -3,6 +3,6 @@ report_type = 'perf'
 show_skipped_groups = True
 tracked_metrics = {
     'test_status': {'metric_name': 'passed', 'displaying_name': 'Passed tests number', 'function': 'match', 'pattern': 'passed', 'displaying_unit': '', 'separation_field': 'tool', 'display_zeros': True},
-    'render_time': {'displaying_name': 'Render time', 'function': 'sum', 'displaying_unit': 'ms', 'separation_field': 'tool'}
+    'render_time': {'displaying_name': 'Inference Time', 'function': 'sum', 'displaying_unit': 'ms', 'separation_field': 'tool'}
 }
 tracked_metrics_files_number = 10000
