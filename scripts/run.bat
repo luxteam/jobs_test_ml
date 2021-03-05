240f078ee95af100c3cfc84f44083d205b6c68d2
Ilia Nemankov <INemankov@luxoft.com>
2021-03-05 12:33:04 +0300

    Fix path to RML

--- scripts/run.bat
+++ scripts/run.bat
@@ -4,4 +4,4 @@ set TESTS_FILTER="%2"
 
 python -m pip install -r ../jobs_launcher/install/requirements.txt
 
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir ML --cmd_variables ResPath "C:\TestResources\rpr_ml_perf_autotests_assets" RMLTool "..\\rpr_ml_au\\test_app.exe" WinMLTool "C:\ML\WinMLRunner\x64\WinMLRunner.exe" TensorRTTool "C:\ML\TensorRT-7.0.0.11\bin\trtexec.exe"
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir ML --cmd_variables ResPath "C:\TestResources\rpr_ml_perf_autotests_assets" RMLTool "..\\RadeonML\\test_app.exe" WinMLTool "C:\ML\WinMLRunner\x64\WinMLRunner.exe" TensorRTTool "C:\ML\TensorRT-7.0.0.11\bin\trtexec.exe"
